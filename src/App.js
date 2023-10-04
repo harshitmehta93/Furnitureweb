1fe1833e65aca5fbd1e8a045b78c719c33a3e198
harshitmehta93 <harshit.mehta@proaxiom.com>
2023-10-04 11:53:26 +0530

    new change

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import '../src/assests/css/bootstrap.min.css'
+import '../src/assests/css/style.css'
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header";
+import Banner from "../components/banner";
+import Product from "../components/Product";
+
+const Home = () => {
+    return(
+        <>
+        <Header/>
+        <Banner/>
+        <Product/>
+        </>
+    )
+}
+export default Home;
